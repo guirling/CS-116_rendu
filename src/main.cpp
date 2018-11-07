8791a3168a18924ab4dc2a74ded2c66ab92b1fd6
Niko Guirlinger <nikoguirlinger@tsf-484-wpa-0-124.epfl.ch>
2018-11-07 16:39:26 +0100

    main

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "network.h"
+#include "random.h"
+#include "simulation.h"
+#include <iostream>
+
+RandomNumbers RNG;
+
+int main(int argc, char **argv) {
+    Network net;
+    Simulation sim(&net);
+    int errcode = 0;
+    try {
+        sim.initialize(argc, argv);
+        sim.run();
+    } catch(TCLAP::ArgException &e) {
+        std::cerr << "Error: " + e.error() + " " + e.argId();
+        errcode = 2;
+    }
+      catch (std::invalid_argument &e) {
+        std::cerr << e.what() << std::endl;
+        errcode = 1;
+    }
+    return errcode;
+}
